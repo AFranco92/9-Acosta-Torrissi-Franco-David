59752825e782c74d5a73f910398facbf714c6d45
Acosta Torrissi, Franco David <afranco.t92@gmail.com>
2017-09-18 23:29:28 -0300

    Comentado de login.php (probando git)

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
-<?php 
+<?php
+/* 
 	require('libs/Smarty.class.php');
 
 	$smarty = new Smarty();
 	$smarty->assign('titulo', 'Ingresar al sistema');
 	$smarty->display('templates/login.tpl');
+
+*/
  ?>
